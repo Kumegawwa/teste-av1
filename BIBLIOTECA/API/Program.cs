d9c5639d79b1852451ab38ca5c5ca06d730e26e9
Kumegawa <lucaskumegawa222@gmail.com>
2025-06-29 22:36:41 -0300

    Registra e mapeia controllers no Program.cs

--- BIBLIOTECA/API/Program.cs
+++ BIBLIOTECA/API/Program.cs
@@ -1,17 +1,17 @@
 using API.Modelos;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// 1. Adicionar o DbContext para injeção de dependência
 builder.Services.AddDbContext<BibliotecaDbContext>();
+
+// 2. Registrar os serviços de Controllers
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
-app.MapGet("/", (BibliotecaDbContext context) =>
-{
-    var categorias = context.Categorias.ToList();
-    return Results.Ok(categorias);
-});
-
+// 3. Mapear os Controllers para que os endpoints fiquem disponíveis
+app.MapControllers();
 
 app.Run();
